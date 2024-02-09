f0732dd5d6cf9ff5b31b82351f79ca0a18aedb5b
Alessandro Gattico <59772116+AlessandroGattico@users.noreply.github.com>
2024-02-09 18:35:20 +0100

    Add files via upload

---
+++ Scrumdinger/Scrumdinger/Models/Theme.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+enum Theme: String, CaseIterable, Identifiable {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    /*
+    var accentColor: Color {
+        switch self {
+            case .bubblegum: return .bubblegum
+            case .buttercup: return .buttercup
+            case .lavender: return .lavender
+            case .orange: return .orange
+            case .periwinkle: return .periwinkle
+            case .poppy: return .poppy
+            case .seafoam: return .seafoam
+            case .sky: return .sky
+            case .tan: return .tan
+            case .teal: return .teal
+            case .yellow: return .yellow
+            case .indigo: return .indigo
+            case .magenta: return .magenta
+            case .navy: return .navy
+            case .oxblood: return .oxblood
+            case .purple: return .purple
+        }
+    }*/
+    var accentColor: Color {
+          switch self {
+          case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+          case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+          }
+      }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+    
+    var name: String {
+        rawValue.capitalized
+    }
+    
+    var id: String{
+        name
+    }
+}
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.sampleData
+    
+    var body: some Scene {
+        WindowGroup {
+            ScrumsView(scrums: $scrums)
+        }
+    }
+}
+
+
+//capitolo 4
+
