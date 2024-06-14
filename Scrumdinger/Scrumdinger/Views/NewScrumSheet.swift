fa594982842e1cd6f73429a8e3afc15f61103f22
alessandrogattico <gattico.alessandro@gmail.com>
2024-06-14 11:11:24 +0200

    Aggiunta capitolo 4

---
+++ Scrumdinger/Scrumdinger/Views/NewScrumSheet.swift
@@ -0,0 +1,18 @@
+//
+//  NewScrumSheet.swift
+//  Scrumdinger
+//
+//  Created by Alessandro Gattico on 14/06/24.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewScrumSheet()
+}
