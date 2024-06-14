d2634619cb4d404851d845b10d352044f66c827d
alessandrogattico <gattico.alessandro@gmail.com>
2024-06-14 11:58:16 +0200

    player

---
+++ Scrumdinger/Scrumdinger/Models/AVPlayer+Ding.swift
@@ -0,0 +1,18 @@
+//
+//  AVPlayer+Ding.swift
+//  Scrumdinger
+//
+//  Created by Alessandro Gattico on 14/06/24.
+//
+
+import SwiftUI
+
+struct AVPlayer_Ding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AVPlayer_Ding()
+}
---
+++ Scrumdinger/Scrumdinger/Views/MeetingFooterView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by Alessandro Gattico on 14/06/24.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingFooterView()
+}
